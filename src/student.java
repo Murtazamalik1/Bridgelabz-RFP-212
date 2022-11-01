08e8fbeef94a3d0439df55684043307d0ffc806f
murtaza <murtazamalik1319@gmail.com>
2022-11-01 17:41:27 +0530

    test

--- src/student.java
+++ src/student.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public interface student {
-}
+interface student {
+
+        public void name();
+        public void roll_no();
+    }
+
+    class s1 implements student {
+        public void name() {
+
+            System.out.println("murtaza malik");
+        }
+        public void roll_no() {
+
+            System.out.println("29");
+        }
+    }
+
+    class Main {
+        public static void main(String[] args) {
+            s1 obj = new s1();  // Create an object
+            obj.name();
+            obj.roll_no();
+        }
+    }
+
